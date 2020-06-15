8a6bc1076b8d80699367dd982dccd4fdb6a63a8b
Supasin Tatiyanupanwong <supasin@tatiyanupanwong.me>
2020-06-15 18:41:20 +0700

    Implemented project skeleton

---
+++ sample/src/main/java/me/tatiyanupanwong/supasin/android/samples/kits/ads/MainActivity.java
@@ -0,0 +1,15 @@
+package me.tatiyanupanwong.supasin.android.samples.kits.ads;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
